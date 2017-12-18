eb8e30150b665530edbaedb7dcd426667f8e10eb
kanika <kanikasethi04@gmail.com>
2017-12-19 00:59:06 +0530

    payment

--- app/controllers/paymentCtrl.js
+++ app/controllers/paymentCtrl.js
@@ -1,2 +1,29 @@
-﻿app.controller('paymentCtrl', function ($scope) {
+﻿app.controller('paymentCtrl', function ($scope, $rootScope, httpService) {
+    $scope.makePayment = function()
+    {
+        $scope.paymentForm.$setSubmitted(true);
+        if ($scope.paymentForm.$valid) {
+            $rootScope.loaderIndicator = true;
+            httpService.makePayment($scope.user).then(function (result) {
+                $rootScope.loaderIndicator = false;
+                commonService.scrollToTop();
+                $scope.paymentForm.$setPristine();
+                $scope.paymentForm.$setUntouched();
+                $scope.successMessageIndicator = true;
+                $scope.errorMessageIndicator = false;
+                $scope.message = "Payment done successfully.";
+                $scope.user = {};
+            });
+        }
+        else {
+            if ($scope.paymentForm.$error.required) {
+                $scope.errorMessageIndicator = true;
+                $scope.message = "Please enter required fields.";
+            }
+            else {
+                $scope.errorMessageIndicator = true;
+                $scope.message = "Your card number is wrong.";
+            }
+        }
+    }
 });
