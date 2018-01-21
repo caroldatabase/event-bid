8b17ef29a7cefc7fdc8f966308e1f3175373a6c4
kanika <kanikasethi04@gmail.com>
2018-01-21 10:58:12 +0530

    eventbid

--- app/controllers/settingsCtrl.js
+++ app/controllers/settingsCtrl.js
@@ -84,10 +84,13 @@
             {
                 $scope.userDetails = response.data.data;
                 var birthday = $scope.userDetails.birthday;
-                birthday = birthday.split("/");
-                $scope.userDetails.day=birthday[0];
-                $scope.userDetails.month=birthday[1];
-                $scope.userDetails.year=birthday[2];
+                if (birthday != null)
+                {
+                    birthday = birthday.split("/");
+                    $scope.userDetails.day = birthday[0];
+                    $scope.userDetails.month = birthday[1];
+                    $scope.userDetails.year = birthday[2];
+                }
                 $scope.portfolioImageArray = [];
                 if ($scope.userDetails && $scope.userDetails.portfolio && ($scope.userDetails.portfolio != "Array" && $scope.userDetails.portfolio != "NULL"))
                 {
