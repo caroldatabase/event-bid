5aebdbfa7099021e2aae68468b16bf5a0c7437e9
kanika <kanikasethi04@gmail.com>
2018-02-13 10:24:53 +0530

    about us

--- app/controllers/aboutusController.js
+++ app/controllers/aboutusController.js
@@ -1,3 +1,8 @@
 ﻿app.controller("aboutusController", ['commonService', function (commonService) {
     commonService.scrollToTop();
+
+
+    $scope.openSignupModal = function () {
+        $('#signUpPopup').modal('toggle');
+    }
 }]);
